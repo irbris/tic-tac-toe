94721aabe60b9d12c3f1aea6c9fde7c0ebc35fe9
irbris <55032489+irbris@users.noreply.github.com>
2019-10-12 15:08:16 +0300

    feat: add task's solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,87 @@
 class TicTacToe {
-    constructor() {
+  constructor() {    
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.сurrentPlayerSymbol = 'x';
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.сurrentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, colIndex) {
+    if (!this.board[rowIndex][colIndex]) {
+      this.board[rowIndex][colIndex] = this.сurrentPlayerSymbol;
+
+      if (this.сurrentPlayerSymbol == 'x') {
+        this.сurrentPlayerSymbol = 'o';
+      } else {
+        this.сurrentPlayerSymbol = 'x';
+      }
     }
+    return this;
+  }
 
-    getCurrentPlayerSymbol() {
-
+  isFinished() {
+    if (this.getWinner() || this.noMoreTurns()) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  getWinner() {
+        var g = this.board;
+        var p1 = 'x';
+        var p2 = 'o';
+        if ((g[0][0] == p1 && g[0][1] == p1 && g[0][2] == p1) ||
+            (g[1][0] == p1 && g[1][1] == p1 && g[1][2] == p1) ||
+            (g[2][0] == p1 && g[2][1] == p1 && g[2][2] == p1) ||
+            (g[0][0] == p1 && g[1][0] == p1 && g[2][0] == p1) ||
+            (g[0][1] == p1 && g[1][1] == p1 && g[2][1] == p1) ||
+            (g[0][2] == p1 && g[1][2] == p1 && g[2][2] == p1) ||
+            (g[0][0] == p1 && g[1][1] == p1 && g[2][2] == p1) ||
+            (g[0][2] == p1 && g[1][1] == p1 && g[2][0] == p1)) {
+            return p1;
+        };
+        if ((g[0][0] == p2 && g[0][1] == p2 && g[0][2] == p2) ||
+            (g[1][0] == p2 && g[1][1] == p2 && g[1][2] == p2) ||
+            (g[2][0] == p2 && g[2][1] == p2 && g[2][2] == p2) ||
+            (g[0][0] == p2 && g[1][0] == p2 && g[2][0] == p2) ||
+            (g[0][1] == p2 && g[1][1] == p2 && g[2][1] == p2) ||
+            (g[0][2] == p2 && g[1][2] == p2 && g[2][2] == p2) ||
+            (g[0][0] == p2 && g[1][1] == p2 && g[2][2] == p2) ||
+            (g[0][2] == p2 && g[1][1] == p2 && g[2][0] == p2)) {
+            return p2;
+        };
+        return null;
+  }
 
+  noMoreTurns() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (!this.board[j][i]) {
+          return false;
+        }
+      }
     }
+    return true;
+  }
 
-    isFinished() {
-
+  isDraw() {
+    if (this.noMoreTurns() && !this.getWinner()) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.board[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
